76f6b15ce7536b5eef3944768d96e37af3ca19ac
czechcoder <p0dsk4lsky@gmail.com>
2023-03-19 18:00:48 +0100

    initial setup

---
+++ MyContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+
+namespace backup_system
+{
+    public class MyContext : DbContext
+    {
+        //public DbSet<Person> Person { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+            string? connectionString = Environment.GetEnvironmentVariable("MY_CONNECTION_STRING");
+
+            if (connectionString == null)
+                throw new InvalidOperationException("MY_CONNECTION_STRING environment variable is not set.");
+
+            optionsBuilder.UseMySQL(connectionString);
+        }
+
+    }
+
+}
