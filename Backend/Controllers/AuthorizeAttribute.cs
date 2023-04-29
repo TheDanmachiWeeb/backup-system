a92d5451317679f9e494fd5136db45152ec4a215
czechcoder <p0dsk4lsky@gmail.com>
2023-04-29 11:52:18 +0200

    deleted wrong folders

---
+++ Backend/Controllers/AuthorizeAttribute.cs
@@ -0,0 +1,31 @@
+﻿using JWT.Algorithms;
+using JWT.Builder;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace BackupSystem.Controllers
+{
+    public class AuthorizeAttribute : Attribute, IAuthorizationFilter
+    {
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            try
+            {
+                string token = context.HttpContext.Request.Headers["Authorization"].ToString().Split(' ').Last();
+
+                var json = JwtBuilder.Create()
+                         .WithAlgorithm(new HMACSHA256Algorithm())
+                         .WithSecret("backpussy69")
+                         .MustVerifySignature()
+                         .Decode(token);
+            }
+            catch
+            {
+                context.Result = new JsonResult(new { message = "Invalid token" })
+                {
+                    StatusCode = StatusCodes.Status401Unauthorized
+                };
+            }
+        }
+    }
+}
