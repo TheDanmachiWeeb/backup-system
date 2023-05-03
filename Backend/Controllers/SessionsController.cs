8188fdd21fd4e28981e49ef5b4614aeeedd6184c
czechcoder <p0dsk4lsky@gmail.com>
2023-05-03 10:49:18 +0200

    sessions controller

--- Backend/Controllers/SessionsController.cs
+++ Backend/Controllers/SessionsController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using JWT.Algorithms;
+using JWT.Builder;
+using Microsoft.AspNetCore.Mvc;
+using BackupSystem.Models;
+using Microsoft.EntityFrameworkCore;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -8,6 +12,7 @@ namespace BackupSystem.Controllers
     [ApiController]
     public class SessionsController : ControllerBase
     {
+        private MyContext context = new MyContext();
         // GET: api/<SessionsController>
         [HttpGet]
         public IEnumerable<string> Get()
@@ -24,8 +29,30 @@ namespace BackupSystem.Controllers
 
         // POST api/<SessionsController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public async Task<ActionResult> Post(User user)
         {
+            try
+            {
+                User login = await this.context.Users.Where(x => x.Username == user.Username).FirstAsync();
+
+                if (login.PasswordHash == user.PasswordHash)
+                {
+                    string token = JwtBuilder.Create()
+                      .WithAlgorithm(new HMACSHA256Algorithm())
+                      .WithSecret("super-secret-foobar")
+                      .AddClaim("exp", DateTimeOffset.UtcNow.AddHours(1).ToUnixTimeSeconds())
+                      .AddClaim("login", user.Username)
+                      .Encode();
+
+                    return Ok(new { token = token });
+                }
+
+                return Unauthorized(new { message = "Invalid credentials" });
+            }
+            catch
+            {
+                return Unauthorized(new { message = "Invalid credentials" });
+            }
         }
 
         // PUT api/<SessionsController>/5
--- Frontend/src/app/pages/login-page/login-page.component.ts
+++ Frontend/src/app/pages/login-page/login-page.component.ts
@@ -1,45 +1,44 @@
-import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup} from "@angular/forms";
-import {SessionsService} from "../../services/sessions.service";
-import {Router} from "@angular/router";
-import {catchError, of} from "rxjs";
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { SessionsService } from '../../services/sessions.service';
+import { Router } from '@angular/router';
+import { catchError, of } from 'rxjs';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.scss'],
 })
-<<<<<<< HEAD
-export class LoginPageComponent {}
-=======
 export class LoginPageComponent implements OnInit {
-
   public form: FormGroup;
   public error: boolean = false;
 
-  public constructor(private fb: FormBuilder,
-                     private router: Router,
-                     private sessions: SessionsService) {
-  }
+  public constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private sessions: SessionsService
+  ) {}
 
   public ngOnInit(): void {
     this.form = this.fb.group({
       username: '',
-      password: ''
+      password: '',
     });
   }
 
   public login(): void {
-    this.sessions.login(this.form.value).pipe(
-      catchError(() => {
-        this.error = true;
-        return of(false);
-      })
-    ).subscribe(result => {
-      if (result) {
-        this.router.navigate([''])
-      }
-    });
+    this.sessions
+      .login(this.form.value)
+      .pipe(
+        catchError(() => {
+          this.error = true;
+          return of(false);
+        })
+      )
+      .subscribe((result) => {
+        if (result) {
+          this.router.navigate(['']);
+        }
+      });
   }
 }
->>>>>>> b59baf86c90090a4ed1efef5199749788f291739
